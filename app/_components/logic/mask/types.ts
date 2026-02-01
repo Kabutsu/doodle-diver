adb4f2447bbeeb44ba0c0ba2a078b53cb33001d8
SLawSystem1 <samuel.law@system1group.com>
2026-02-01 10:17:00 +0000

    Fixed masks

--- app/_components/logic/mask/types.ts
+++ app/_components/logic/mask/types.ts
@@ -11,17 +11,17 @@ export interface MaskDef {
 
 export const MASK_DEFS: Record<MaskType, MaskDef> = {
   pressure: {
-    durationMin: 8,
-    durationMax: 12,
-    fallSpeedMult: 0.7,
-    oxygenDrainMult: 1,
-    bounceDecayMult: 1.4,
+    durationMin: 5,
+    durationMax: 10,
+    fallSpeedMult: 2.5,
+    oxygenDrainMult: 2,
+    bounceDecayMult: 0.7,
     pauseOxygenDrain: false,
   },
   rebreather: {
-    durationMin: 8,
+    durationMin: 6,
     durationMax: 12,
-    fallSpeedMult: 1.3,
+    fallSpeedMult: 0.5,
     oxygenDrainMult: 0.5,
     bounceDecayMult: 1,
     pauseOxygenDrain: false,
@@ -29,7 +29,7 @@ export const MASK_DEFS: Record<MaskType, MaskDef> = {
   blind: {
     durationMin: 8,
     durationMax: 12,
-    fallSpeedMult: 1,
+    fallSpeedMult: 1.3,
     oxygenDrainMult: 1,
     bounceDecayMult: 1,
     pauseOxygenDrain: true,
